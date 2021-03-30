9af5a4b891aa9a30dd9f9a0ab02e9f1330f0f5f3
LordSquishers <lordsquisherton@gmail.com>
2021-03-30 17:46:07 +0100

    pretty much working now

--- test_vertcalc.py
+++ test_vertcalc.py
@@ -4,17 +4,25 @@ import matplotlib.pyplot as plt
 # x,y of panel joints
 # top right (1,1,z)
 # top left (-1,1,z)
-# bottom middle (x,-0.5,z)
+# bottom middle (x,-0.75,z)
+
+# example crate
 xy_pts = np.array([
-    [1, 1],
-    [-1, 1],
-    [0,-0.5]
+    [40., 15.],
+    [-40., 15.],
+    [0.,-10.]
 ])
 
-z_error_margin = 0.1  # meters? anything really
-x_margin = 0.05
+# square
+xy_pts = np.array([
+    [1., 1.],
+    [-1., 1.],
+    [0.,-0.75]
+])
+
+z_error_margin = 0.1  # meters? anything really lowest z value
 x_step_size = 0.01
-max_actuator_height = 1  # this is self explanatory
+max_actuator_height = 1.5  # this is self explanatory (meters)
 
 def calculate_normal(azimuth, elevation):
     # radians to degrees
@@ -28,18 +36,16 @@ def calculate_normal(azimuth, elevation):
     return np.array([x, y, z])
 
 
-def find_z_adjustable(xy, z, plane_eq, max_xs):
+def find_z_adjustable(y, plane_eq, max_xs):
     a = plane_eq[0]
     b = plane_eq[1]
     c = plane_eq[2]
     d = plane_eq[3]
-    y = xy[1]
 
     best_min_x = 9999
     best_max_x = -9999
-    x_g = np.arange(-1, 1, 0.01)  # np.arange(max_xs[0], max_xs[1], x_step_size)
-    print(x_g.min())
-    z_g = x_g
+    x_g = np.arange(max_xs[0], max_xs[1], x_step_size)
+    z_g = np.copy(x_g)
 
     idx = 0
     for x_pt in x_g:
@@ -50,14 +56,16 @@ def find_z_adjustable(xy, z, plane_eq, max_xs):
         if z_val > z_error_margin and z_val < max_actuator_height:
             best_max_x = max(x_val, best_max_x)
             best_min_x = min(x_val, best_min_x)
+    # print('midpoint x', 0, 'mid z', -(a*0 + b*y + d) / c)
 
-    print('min x', best_min_x, 'min z',-(a*best_min_x + b*y + d) / c, 'max x', best_max_x, 'max z', -(a*best_max_x + b*y + d) / c)
-    print('midpoint x', 0.5, 'mid z', -(a*0.5 + b*y + d) / c)
+    # plt.plot(x_g, z_g)
+    # plt.show()
 
-    plt.plot(x_g, z_g)
-    print(x_g.min())
-    plt.show()
-
+    new_x = (best_max_x + best_min_x) / 2
+    new_z = -(a * new_x + b * y + d) / c
+    # print(new_x, new_z)
+    # print(best_min_x, best_max_x)
+    return new_x, new_z
 
 def calculate_lengths(normal):
     unit_normal = normal / normal.sum()
@@ -68,7 +76,7 @@ def calculate_lengths(normal):
     b = unit_normal[1]
     c = unit_normal[2]
     d = -(a * p0[0] + b * p0[1] + c * p0[2])
-    print('eq:', np.array([a, b, c, d]))
+    print('Plane Equation:', np.array([a, b, c, d]))
 
     # EVALUATE AND CORRECT Z COORDS
     z_pts = []
@@ -79,18 +87,31 @@ def calculate_lengths(normal):
 
         z_pts.append(z)
 
-    # if z is less than 0, it means we're clipping into the container.
-    # increase the midpoint by the amount we're clipping plus MoE
     z_pts = np.array(z_pts)  # convert to numpy
-    # z_offset = -z_pts.min() + z_error_margin  # lowers to safety margin
-    # z_pts += z_offset
 
     # NEW #
     # adjustable bottom point #
-    z_bot = find_z_adjustable(xy_pts[2], z_pts[2], [a, b, c, d], [xy_pts[1][0], xy_pts[0][0]])
+    x, z = find_z_adjustable(xy_pts[2][1], [a, b, c, d], [xy_pts[1][0], xy_pts[0][0]])
+    xy_pts[2][0] = x
+    z_pts[2] = z
 
-    print('TR, TL, BMID')
-    print(z_pts)
+    # if z is less than 0, it means we're clipping into the container.
+    # increase the midpoint by the amount we're clipping plus MoE
+    # this lowers the whole panel to the minimum to conserve space.
+    z_offset = -z_pts.min() + z_error_margin  # lowers to safety margin
+    z_pts += z_offset
+
+    ps = []
+    for idx in range(len(z_pts)):
+        x = xy_pts[idx][0]
+        y = xy_pts[idx][1]
+        z = z_pts[idx]
+        ps.append([x, y, z])
+        idx += 1
+    ps = np.array(ps)
+
+    print('Points:')
+    print(ps)
 
     # max checking and output of linear act. lengths
     if z_pts.max() > max_actuator_height:
@@ -101,25 +122,13 @@ def calculate_lengths(normal):
 
 
 # inputs in degrees (NYC at Noon, march 29th 2021)
-azimuth = 155.82
-elevation = 50.45
+azimuth = 250.07
+elevation = 60
 
 n = calculate_normal(azimuth, elevation)
 # n = np.array([0, 0, 1])
 zs = calculate_lengths(n)
 
-ps = []
-for idx in range(len(xy_pts)):
-    x = xy_pts[idx][0]
-    y = xy_pts[idx][0]
-    z = zs[idx]
-    ps.append([x, y, z])
-
-ps = np.array(ps)
-peri = 0  # TODO verify perimeter calculations -- this is to verify pt calculations
-for idx in range(len(xy_pts)):
-    diff = ps[idx] - ps[(idx + 1) % len(xy_pts)]
-    # print(diff)
-    peri += np.linalg.norm(diff)
-
-print('Permieter', round(peri,3), 'm')
+# issues:
+# large rectangular panels have a hard time tracking degrees < 80
+# max height?
